7f44b5397d8e097586f574873ee9f0580219ee1b
Gutowski, Michal <michal.gutowski@dell.com>
2025-10-01 23:05:57 +0200

    fixes

--- src/app/MyRuntimeProvider.tsx
+++ src/app/MyRuntimeProvider.tsx
@@ -23,10 +23,12 @@ const MyModelAdapter: ChatModelAdapter = {
       headers: {
         "Content-Type": "application/json",
       },
+      
       // forward the messages in the chat to the API
       body: JSON.stringify({
-        text: messages[messages.length-1].content[0].text, // forward only last message
+        text: messages[messages.length-1].content[0] as TextMessagePart, // forward only last message
       }),
+      
       // if the user hits the "cancel" button or escape keyboard key, cancel the request
       signal: abortSignal,
       });
